e84c28278f8ea7a93af24044153e57e696f341ad
INDRA-011 <indr.yznn.77@gmail.com>
2026-06-17 13:25:38 +0545

    basic codes

---
+++ jun12.py
@@ -0,0 +1,54 @@
+
+# HW
+# interpreter and compiler: compare, differentiate
+
+# Parameterized function example
+'''
+MAX_SCORE = 100
+
+def calculate_grade(score, max_score = MAX_SCORE):
+    """Return a letter grade for the given score."""
+    percentage = (score / max_score) * 100
+
+    if percentage >= 90:
+        return "A"
+    elif percentage >= 75:
+        return "B"
+    else:
+        return "C"
+
+print(calculate_grade(85))
+
+name = "indra"
+age = 20
+roll_no = "LC00017003423"
+gpa = 3.6
+is_enrolled = True
+
+print(name)
+print(age)
+print(roll_no)
+print(gpa)
+print(is_enrolled)
+
+
+# Fast Assignment 
+a, b, c = 1, 2, 3
+
+print(a,c)
+'''
+
+
+'''
+def greet(name):
+   """Return a greeeting strgin for the given name."""
+   return f'hello, {name}'
+
+print(greet('indra'))
+'''
+
+import keyword
+
+print(keyword.kwlist)
+print(keyword)
+
---
+++ jun17.py
@@ -0,0 +1,6 @@
+def greet(name):
+    '''Return a greeting string for the given name.'''
+    return f'Hello, {name}!'
+
+print(greet('Alice'))
+print(greet.__doc__)
